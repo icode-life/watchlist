33d6280f293df59658bf9f9f23c1087356d12265
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-09-18 22:24:13 +0200

    scaffolding a single view without crud

---
+++ Watchlist/Controllers/WatchlistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Watchlist.Data;
+using Watchlist.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Watchlist.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public WatchlistController(ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}
+
